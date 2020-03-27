ac08d2271c08a1ce6dbd5ec966a6fa69e51cb95d
nfmohit-wpmudev <nfmohit49@gmail.com>
2020-03-27 22:58:09 +0600

    Set up ESLint loader

--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,12 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				enforce: 'pre',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader'
+			},
+			{
 				test: /\.js$/,
 				exclude: /node_modules/,
 				loader: 'babel-loader',
